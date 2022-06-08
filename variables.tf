c89580e2e80ef789a906a16d757114f84ca0c893
Ahmed Fourat TOUZRI <ahmedfourat.touzri@partner.bmwgroup.com>
2022-06-08 21:17:57 +0100

    updated azuread & azurerm providers authentication method

--- variables.tf
+++ variables.tf
@@ -1,8 +1,30 @@
+# Azure subscription ID
+variable "subscription_id" {
+  type        = string
+  description = "Azure account subscription ID"
+  default     = "53b8ff38-9c35-473b-9fc9-9562f9da43db"
+  sensitive   = true
+}
+
 # Azure tenant ID
 variable "tenant_id" {
   type        = string
   description = "Azure Service Principal tenant ID"
   default     = "470498c0-975f-4d24-9391-3e4be3c30fd2"
+}
+
+# Azure client ID
+variable "client_id" {
+  type        = string
+  description = "Azure Service Principal client ID"
+  default     = "7f322fe4-a924-4e18-a61e-dfb47b566efa"
+}
+
+# Azure client secret
+variable "client_secret" {
+  type        = string
+  description = "Azure Service Principal client secret"
+  sensitive   = true
 }
 
 # Azure Location
